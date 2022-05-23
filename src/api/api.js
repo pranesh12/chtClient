5f8cb7a793ee18140b7d8f17b9936f5d667a05db
pranesh12 <praneshchakama6@gmail.com>
2022-05-23 23:24:23 +0600

    make it better

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // export const url = `http://localhost:5000/`;
-//"https://radiant-atoll-98368.herokuapp.com/"
+//
 //localhost:5000/
 //https://chtbd.herokuapp.com/
-export const url = `http://localhost:5000/`;
+export const url = "https://radiant-atoll-98368.herokuapp.com/";
