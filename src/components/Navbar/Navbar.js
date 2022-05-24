de98d8162714c00ab0291e78d85ae95fdd190834
pranesh12 <praneshchakama6@gmail.com>
2022-05-24 16:39:51 +0600

    udpaded data

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                         className="nav-link"
                         to="/Education/61853147b55f903ed872b2e7"
                       >
-                        t2
+                        t𑄘𑄳𑄃𑄬𑄣𑄴
                       </Link>
                     </li>
                     <li className="nav-item">
